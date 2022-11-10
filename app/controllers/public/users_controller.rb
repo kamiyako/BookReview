e9d418efb2f1045782f96a5cc5081fd980459846
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-11-10 09:53:40 +0000

    退会機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,14 +1,26 @@
 class Public::UsersController < ApplicationController
-  
+  before_action :set_user, :only => [:show, :destroy]
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews
   end
 
+  # def destroy
+  #   @review = Review.find(params[:id])
+  #   @review.destroy
+  # end
+  
   def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
+    @user = User.find(params[:id]) 
+    @user.destroy
+    flash[:notice] = 'ユーザーを削除しました。'
+    redirect_to :root
   end
-
+  
+  private
+  
+  def set_user
+    @user = User.find_by(:id => params[:id])
+  end
 
 end
