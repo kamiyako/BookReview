ff9aa3d669ff17e78bfebdadc8abb41b91df57c7
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-11-16 13:24:44 +0000

    [bug]tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,13 @@
 class Tag < ApplicationRecord
   has_many :book_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :reviews, through: :book_tags
+  
+  
+
+  # def name_tag(str)
+  #   require 'nkf'
+  #   str = "アイウエオＡＢＣ０１２"
+  #   # nkfライブラリを使用して変換を実行
+  #   p NKF.nkf('-w -Z4', str)
+  # end
 end
